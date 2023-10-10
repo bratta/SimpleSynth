d71c01aaf70e278bd296d303a423fc21c44cb269
Tim Gourley <tgourley@gmail.com>
2023-10-10 10:12:20 -0500

    Initial Commit

---
+++ SimpleSynth/SimpleSynthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSynthApp.swift
+//  SimpleSynth
+//
+//  Created by Tim Gourley on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSynthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
